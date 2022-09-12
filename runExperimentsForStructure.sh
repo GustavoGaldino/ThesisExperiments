e3e3bea9da91dc9fcb82f84ef990f470baf28511
LeonardoGaldino <lcgm@cin.ufpe.br>
2022-09-13 01:04:28 +0200

    Make run script depend on env variable

--- runExperimentsForStructure.sh
+++ runExperimentsForStructure.sh
@@ -1,5 +1,5 @@
 
-./dist-newstyle/build/x86_64-linux/ghc-7.11.20150126/tg-0.1.0.0/x/tg/build/tg/tg $1
+./dist-newstyle/build/x86_64-linux/ghc-$GHC_V/tg-0.1.0.0/x/tg/build/tg/tg $1
 exec_time=$(cat tg.prof | grep "total time"  | cut -d"=" -f2 | cut -d"s" -f1 | sed "s/ //g" | sed "s/,//g")
 exec_memory=$(cat tg.prof | grep "total alloc" | cut -d"=" -f2 | cut -d"b" -f1 | sed "s/ //g" | sed "s/,//g")
 exec_energy=$(cat tg.prof | grep "total energy" | cut -d"=" -f2 | cut -d"j" -f1 | sed "s/ //g" | sed "s/,//g")
