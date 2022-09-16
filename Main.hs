d923c9f2974a37b76137f794a0c69ea24676bc7b
LeonardoGaldino <lcgm@cin.ufpe.br>
2022-09-16 10:04:04 +0200

    Add some multiplexing to which data strucutre/experiment will be ran

--- Main.hs
+++ Main.hs
@@ -1,24 +1,41 @@
-
-import qualified Data.Edison.Seq.SimpleQueue as S
 import System.Environment
 
+import qualified Data.Edison.Seq.SimpleQueue as S
+
+
+data EdisonDataStructure = EdisonSeq deriving Show
+data EdisonExperiment = Add deriving Show
+
+dataStructureAndExperimentToExperimentFunction :: EdisonDataStructure -> EdisonExperiment -> IO ()
+dataStructureAndExperimentToExperimentFunction EdisonSeq Add = seqAddExperiment
+
+rawStringToEdisonDataStructure :: String -> EdisonDataStructure
+rawStringToEdisonDataStructure "EdisonSeq" = EdisonSeq
+rawStringToEdisonDataStructure _ = error "Could not match any edison data structure"
+
+rawStringToEdisonExperiment :: String -> EdisonExperiment
+rawStringToEdisonExperiment "Add" = Add
+rawStringToEdisonExperiment _ = error "Could not match any edison experiment"
+
 baseNElems :: Int
-baseNElems = 100000000
+baseNElems = 100000
 
 addFromNElems :: Int
-addFromNElems = 100000000
+addFromNElems = 100000
 
 addEnvSetup :: S.Seq Int
 addEnvSetup = defaultEnv
 
-main2 :: IO ()
-main2 = do
-    upperl <- map (\v -> read v :: Int) `fmap` getArgs
-    let upper = head upperl
-    return (S.lhead $ temp upper) >>= putStrLn . show
-
 main :: IO ()
-main = benchmark `seq` return ()
+main = do
+    args <- getArgs
+    let ds = rawStringToEdisonDataStructure $ head args
+    let experiment = rawStringToEdisonExperiment $ args !! 1
+    seq (dataStructureAndExperimentToExperimentFunction ds experiment) (return ())
+    --return (S.lhead $ temp upper) >>= putStrLn . show
+
+seqAddExperiment :: IO ()
+seqAddExperiment = benchmark `seq` return ()
     where
         ds = addEnvSetup
         benchmark = addNDistinctFrom ds addFromNElems 0
@@ -38,3 +55,4 @@ addNDistinctFrom seq n m =
 
 defaultEnv :: S.Seq Int
 defaultEnv = addNDistinctFrom S.empty baseNElems 0
+
