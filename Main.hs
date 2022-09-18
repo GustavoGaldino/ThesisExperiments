c24e97cfe0496b52b859d2d4f7dc54ff860e30cf
LeonardoGaldino <lcgm@cin.ufpe.br>
2022-09-18 12:19:54 +0200

    Add AddAll experiment for Seq

--- Main.hs
+++ Main.hs
@@ -4,10 +4,7 @@ import Control.DeepSeq
 import qualified Data.Edison.Seq.SimpleQueue as S
 
 data DataStructure = Seq deriving Show
-data Experiment = Add deriving Show
-
-dataStructureAndExperimentToExperimentFunction :: DataStructure -> Experiment -> IO ()
-dataStructureAndExperimentToExperimentFunction Seq Add = seqAddExperiment
+data Experiment = Add | AddAll deriving Show
 
 rawStringToDataStructure :: String -> DataStructure
 rawStringToDataStructure "Seq" = Seq
@@ -15,7 +12,12 @@ rawStringToDataStructure _ = error "Could not match any data structure"
 
 rawStringToExperiment :: String -> Experiment
 rawStringToExperiment "Add" = Add
+rawStringToExperiment "AddAll" = AddAll
 rawStringToExperiment _ = error "Could not match any experiment"
+
+dataStructureAndExperimentToExperimentFunction :: DataStructure -> Experiment -> IO ()
+dataStructureAndExperimentToExperimentFunction Seq Add = seqAddExperiment
+dataStructureAndExperimentToExperimentFunction Seq AddAll = seqAddAllExperiment
 
 baseNElems :: Int
 baseNElems = 100000
@@ -23,8 +25,11 @@ baseNElems = 100000
 addFromNElems :: Int
 addFromNElems = 100000
 
-addEnvSetup :: S.Seq Int
-addEnvSetup = defaultEnv
+addAllFromNElems :: Int
+addAllFromNElems  = 1000
+
+addAllNRepeats :: Int
+addAllNRepeats = 1000
 
 instance (NFData a) => NFData (S.Seq a) where
     rnf s = S.strictWith rnf s `seq` ()
@@ -43,6 +48,12 @@ seqAddExperiment = benchmark `deepseq` return ()
         ds = addEnvSetup
         benchmark = addNDistinctFrom ds addFromNElems 0
 
+seqAddAllExperiment :: IO ()
+seqAddAllExperiment = benchmark `deepseq` return ()
+    where
+        (s, t) = addAllEnvSetup
+        benchmark = addAllNTimes s t addAllNRepeats
+
 addNDistinctFrom :: S.Seq Int -> Int -> Int -> S.Seq Int
 addNDistinctFrom seq 0 _ = seq
 addNDistinctFrom seq n m =
@@ -53,6 +64,18 @@ addNDistinctFrom seq n m =
     in
         addNDistinctFrom ( elemToAdd `cons` seq ) nextNumber m
 
+addAll :: S.Seq Int -> S.Seq Int -> S.Seq Int
+addAll = S.append
+
+addAllNTimes :: S.Seq Int -> S.Seq Int -> Int -> S.Seq Int
+addAllNTimes s _ 0 = s
+addAllNTimes s t n = deepseq ( addAll s t ) ( addAllNTimes s t ( n - 1 ) )
+
 defaultEnv :: S.Seq Int
 defaultEnv = addNDistinctFrom S.empty baseNElems 0
 
+addAllEnvSetup :: (S.Seq Int, S.Seq Int)
+addAllEnvSetup = (defaultEnv, addNDistinctFrom S.empty addAllFromNElems 0)
+
+addEnvSetup :: S.Seq Int
+addEnvSetup = defaultEnv
